d1d0c46419bf16e294ddf4e28274940a59e5702d
Jabou19 <73283747+Jabou19@users.noreply.github.com>
2023-05-30 13:51:42 +0200

    Update module-info.java

--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -1,13 +1,11 @@
 import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
-import dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
-import dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
 
 module Player {
     requires Common;
     requires CommonBullet;
     uses BulletSPI;
-    provides IEntityProcessingService with PlayerControlSystem;
-    provides IGamePluginService with PlayerPlugin;
+     provides IEntityProcessingService with dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
+    provides IGamePluginService with dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
 }
